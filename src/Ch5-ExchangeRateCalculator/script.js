7d021a6345a4448d785f33a0a8ade30423727c56
chidraeve <lukas.mainka@clubitsolutions.de>
2020-10-14 18:58:22 +0200

    25. Fetch Rates & Update DOM

--- src/Ch5-ExchangeRateCalculator/script.js
+++ src/Ch5-ExchangeRateCalculator/script.js
@@ -1,5 +1,39 @@
-function calculate() {
+const currencyEl_one = document.getElementById('currency-one');
+const amountEl_one = document.getElementById('amount-one');
+const currencyEl_two = document.getElementById('currency-two');
+const amountEl_two = document.getElementById('amount-two');
 
+const rateEl = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+// Fetch exchange rates und update the DOM
+function calculate() {
+	const currency_one = currencyEl_one.value;
+	const currency_two = currencyEl_two.value;
+
+	fetch(`https://v6.exchangerate-api.com/v6/14a5fb2213c652fe9e227f3f/latest/${currency_one}`)
+		.then(res => res.json())
+		.then(data => {
+			// console.log(data);
+			const rate = data.conversion_rates[currency_two];
+
+			rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+
+			amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+		})
 }
+
+// Event Listeners
+currencyEl_one.addEventListener('change', calculate);
+amountEl_one.addEventListener('input', calculate);
+currencyEl_two.addEventListener('change', calculate);
+amountEl_two.addEventListener('change', calculate);
+
+swap.addEventListener('click', () => {
+    const temp = currencyEl_one.value;
+    currencyEl_one.value = currencyEl_two.value;
+    currencyEl_two.value = temp;
+    calculate();
+})
 
 calculate();
