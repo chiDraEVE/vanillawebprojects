30dfd1aab8fbba5c059fc5b355b681c213ca2c51
chidraeve <lukas.mainka@clubitsolutions.de>
2021-01-20 20:45:33 +0100

    29. Output Users - forEach() & DOM Methods

--- src/Ch6-DOMArrayMethods/script.js
+++ src/Ch6-DOMArrayMethods/script.js
@@ -13,7 +13,7 @@ getRandomUser()
 
 // fetch random user and add money
 async function getRandomUser() {
-	const res = await fetch('https://randomuser.me/api')
+	const res = await fetch('https://api.randomuser.me', {headers: { 'Access-Control-Allow-Origin': '*'}})
 	const data = await res.json()
 
 	const user = data.results[0]
@@ -29,4 +29,27 @@ async function getRandomUser() {
 // Add new obj to data arr
 function addData(obj) {
 	data.push(obj)
+
+	updateDOM()
 }
+
+// Update DOM
+function updateDOM(providedData = data) {
+	// Clear main div
+	main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>'
+
+	providedData.forEach(item => {
+		const element = document.createElement('div')
+		element.classList.add('person')
+		element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(item.money)}`
+		main.appendChild(element)
+	})
+}
+
+// Format number as money
+function formatMoney(number) {
+	return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')
+}
+
+// Event Listeners
+addUserBtn.addEventListener('click', getRandomUser)
