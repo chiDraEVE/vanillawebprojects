4d757acf04ea2bbf26522cb014605058e7dbb93d
chidraeve <lukas.mainka@clubitsolutions.de>
2021-01-23 19:35:14 +0100

    44. Letter Press Event Handler

--- src/Ch8-HangmanGame/script.js
+++ src/Ch8-HangmanGame/script.js
@@ -28,12 +28,98 @@ function displayWord() {
 	}
 	`
 	const innnerWord = wordEl.innerText.replace(/\n/g, '')
-	console.log(wordEl.innerText, innnerWord)
 
 	if (innnerWord === selectedWord) {
 		finalMessage.innerText = 'Congratulations! You won! 😃'
 		popup.style.display = 'flex'
 	}
 }
+
+// Update the wrong letters
+function updateWrongLettersEl() {
+	console.log('Update wrong')
+}
+
+// Show notification
+function showNotification() {
+	notification.classList.add('show')
+
+	setTimeout(() => notification.classList.remove('show'), 2000)
+}
+
+// Keydown letter press
+window.addEventListener('keypress', e => {
+	const handleLetter = letter => {
+		// console.log(letter)
+		if (selectedWord.includes(letter)) {
+			if (!correctLetters.includes(letter)) {
+				correctLetters.push(letter)
+				displayWord()
+			} else {
+				showNotification()
+			}
+		} else {
+			if (!wrongLetters.includes(letter)) {
+				wrongLetters.push(letter)
+				updateWrongLettersEl()
+			} else {
+				showNotification()
+			}
+		}
+	}
+
+	if (e.defaultPrevented) {
+		return; // Should do nothing if the default action has been cancelled
+	}
+
+	let handled = false;
+	if (e.key !== undefined) {
+		// Handle the event with KeyboardEvent.key and set handled true.
+		handled = true
+		if (e.key >= 'A' && e.key <= 'z') {
+			handleLetter(e.key)
+		}
+	} else if (e.keyCode !== undefined) {
+		// Handle the event with KeyboardEvent.keyCode and set handled true.
+		handled = true
+		if (e.keycode >= 65 && e.keycode <= 90) {
+			handleLetter(e.key)
+		}
+	}
+
+	if (handled) {
+		// Suppress "double action" if event handled
+		e.preventDefault();
+	}
+
+	/** Brad uses keycode which is deprecated. Normally you could use key instead, but not all browser support this
+	 * @url https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/keyCode
+	 * I'll use Brad's code in the else if block
+	 * I am also using 'keypress' instead of 'keypress' like Brad does. So it's possible to handle characters with
+	 * shift pressed or capslock on.
+	 * there's an example for dealing key and keycode:
+	 */
+
+	/**
+	 * window.addEventListener("keydown", function (event) {
+	 * if (event.defaultPrevented) {
+	 *   return; // Should do nothing if the default action has been cancelled
+	 * }
+	 *
+	 * var handled = false;
+	 * if (event.key !== undefined) {
+	 *   // Handle the event with KeyboardEvent.key and set handled true.
+	 * } else if (event.keyCode !== undefined) {
+	 *   // Handle the event with KeyboardEvent.keyCode and set handled true.
+	 * }
+	 *
+	 * if (handled) {
+	 *   // Suppress "double action" if event handled
+	 *   event.preventDefault();
+	 * }
+	 *}, true);
+	 *
+	 */
+})
 
 displayWord()
