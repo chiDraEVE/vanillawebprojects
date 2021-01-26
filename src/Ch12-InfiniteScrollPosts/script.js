7cbdbced9623b4abe32988b370e23ba4186ee179
chidraeve <lukas.mainka@clubitsolutions.de>
2021-01-26 14:36:42 +0100

    68. Add Infinite Scroll

--- src/Ch12-InfiniteScrollPosts/script.js
+++ src/Ch12-InfiniteScrollPosts/script.js
@@ -35,5 +35,26 @@ async function showPosts() {
   })
 }
 
+// Show loader & fetch more posts
+function showLoading() {
+  loading.classList.add("show")
+
+  setTimeout(() => {
+    loading.classList.remove("show")
+
+    setTimeout(() => {
+      page++
+      showPosts()
+    }, 300)
+  }, 1000)
+}
+
 // Show initial posts
 showPosts()
+
+// Event Listeners
+window.addEventListener("scroll", () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement
+
+  if (scrollTop + clientHeight >= scrollHeight - 5) showLoading()
+})
