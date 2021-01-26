d0a62e1fdf61fd3bfe0fd58c2cbadb3e0c374856
chidraeve <lukas.mainka@clubitsolutions.de>
2021-01-26 15:23:25 +0100

    69. Filter Fetched Posts

--- src/Ch12-InfiniteScrollPosts/script.js
+++ src/Ch12-InfiniteScrollPosts/script.js
@@ -11,9 +11,7 @@ async function getPosts() {
     `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
   )
 
-  const data = await res.json()
-  console.log(data)
-  return data
+  return await res.json()
 }
 
 // Show posts in DOM
@@ -49,6 +47,23 @@ function showLoading() {
   }, 1000)
 }
 
+// Filter posts by input
+function filterPosts(e) {
+  const term = e.target.value.toUpperCase()
+  const posts = document.querySelectorAll(".post")
+
+  posts.forEach((post) => {
+    const title = post.querySelector(".post-title").innerText.toUpperCase()
+    const body = post.querySelector(".post-body").innerText.toUpperCase()
+
+    if (title.indexOf(term) > -1 || body.indexOf(term) > -1) {
+      post.style.display = "flex"
+    } else {
+      post.style.display = "none"
+    }
+  })
+}
+
 // Show initial posts
 showPosts()
 
@@ -58,3 +73,5 @@ window.addEventListener("scroll", () => {
 
   if (scrollTop + clientHeight >= scrollHeight - 5) showLoading()
 })
+
+filter.addEventListener("input", filterPosts)
